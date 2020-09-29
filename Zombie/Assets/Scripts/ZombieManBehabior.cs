8817f7a79e2ab4840a6f99da1e050406fe2a28f2
Nazmul Hasan <nazmulimm@gmail.com>
2020-09-29 18:51:11 +0600

    Final Update

---
+++ Zombie/Assets/Scripts/ZombieManBehabior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManBehabior : MonoBehaviour
+{
+    public GameObject zombie;
+    public GameObject human;
+    Animator aniZombie, aniHuman;
+    Transform transZombie, transHuman;
+
+    bool zTrack, hTrack;
+
+    private void Start()
+    {
+        aniZombie = zombie.GetComponentInChildren<Animator>();
+        aniHuman = human.GetComponentInChildren<Animator>();
+        transZombie = zombie.GetComponent<Transform>();
+        transHuman = human.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+        if(zTrack && hTrack)
+        {
+            Vector3 zom = transHuman.position - transZombie.position;
+            Quaternion zombieRotation = Quaternion.LookRotation(zom);
+            
+            transZombie.rotation = zombieRotation;
+
+            transHuman.rotation = zombieRotation;
+
+
+            aniZombie.Play("run");
+            aniHuman.Play("run");
+        }
+        else
+        {
+            aniZombie.Play("idle");
+            aniHuman.Play("idle");
+        }
+        
+    }
+
+    public void ZombieTracked()
+    {
+        zTrack = true;
+    }
+    public void ZombieLost()
+    {
+        zTrack = false;
+    }
+
+    public void HumanTracked()
+    {
+        hTrack = true;
+    }
+    public void HumanLost()
+    {
+        hTrack = false;
+    }
+}
